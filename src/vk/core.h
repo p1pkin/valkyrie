09b624a0c6e938de6bcf98ea0dd7551e4b77d5ba
Stefano Teso <stefano.teso@gmail.com>
2013-07-31 11:41:15 +0200

    vk/core: expose vk_util_get_int_option().

--- src/vk/core.h
+++ src/vk/core.h
@@ -272,6 +272,7 @@ bool	is_valid_mat4x3f (mtx4x3f_t mtx);
 bool	is_valid_mat4x4f (mtx4x4f_t mtx);
 
 bool	vk_util_get_bool_option (const char *name, bool fallback);
+int	vk_util_get_int_option (const char *name, int fallback);
 
 void	*vk_load_any (const char *path, size_t *_size);
 
