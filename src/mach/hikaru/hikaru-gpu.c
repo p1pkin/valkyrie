p1pkin/valkyrie

commit ea8ebf904dc2da8805e3a7a342ba3f194df657f5

hikaru,hikaru/gpu: move 00400000 MMIO to the GPU.
It looks like it resets the GPU hardware or something.

src/mach/hikaru/hikaru-gpu.c
@@ -1006,12 +1006,16 @@ static int
 hikaru_gpu_get (vk_device_t *dev, unsigned size, uint32_t addr, void *val)
 {
 	hikaru_gpu_t *gpu = (hikaru_gpu_t *) dev;
+	uint16_t *val16 = (uint16_t *) val;
 	uint32_t *val32 = (uint32_t *) val;
 
-	VK_ASSERT (size == 4 || (size == 2 && addr == 0x15000010));
+	VK_ASSERT (size == 4 ||
+	           (size == 2 && (addr == 0x15000010 || addr == 0x00400000)));
 
 	*val32 = 0;
-	if (addr >= 0x15000000 && addr < 0x15000100) {
+	if (addr == 0x00400000) {
+		*val16 = gpu->unk_00400000;
+	} else if (addr >= 0x15000000 && addr < 0x15000100) {
 		switch (addr & 0xFF) {
 		case 0x10:
 			if (size == 2) {
@@ -1062,9 +1066,12 @@ hikaru_gpu_put (vk_device_t *device, unsigned size, uint32_t addr, uint64_t val)
 {
 	hikaru_gpu_t *gpu = (hikaru_gpu_t *) device;
 
-	VK_ASSERT (size == 4);
+	VK_ASSERT (size == 4 ||
+	           (size == 2 && addr == 0x00400000));
 
-	if (addr >= 0x15000000 && addr < 0x15000100) {
+	if (addr == 0x00400000) {
+		gpu->unk_00400000 = val;
+	} else if (addr >= 0x15000000 && addr < 0x15000100) {
 		switch (addr & 0xFF) {
 		case 0x00:
 		case 0x04:

src/mach/hikaru/hikaru.c
@@ -263,9 +263,6 @@ unk_m_get (vk_device_t *dev, unsigned size, uint32_t addr, void *val)
 	VK_ASSERT (size == 2);
 
 	switch (addr) {
-	case 0x00400000:
-		*val16 = hikaru->unk00400000_m;
-		break;
 	case 0x01000000:
 		*val16 = hikaru->unk01000000_m;
 		break;
@@ -286,9 +283,6 @@ unk_m_put (vk_device_t *dev, unsigned size, uint32_t addr, uint64_t val)
 	VK_ASSERT (size == 2);
 
 	switch (addr) {
-	case 0x004000000:
-		hikaru->unk00400000_m = val;
-		break;
 	case 0x01000000:
 		hikaru->unk01000000_m = val;
 		break;
@@ -523,7 +517,6 @@ hikaru_reset (vk_machine_t *mach, vk_reset_type_t type)
 	vk_device_reset (hikaru->aica_s, type);
 	vk_device_reset (hikaru->gpu, type);
 
-	hikaru->unk00400000_m = 0;
 	hikaru->unk01000000_m = 0;
 	hikaru->unk01000100_m = 0;
 	hikaru->unk1A800008_s = 0xFFFF;
@@ -638,9 +631,9 @@ setup_master_mmap (hikaru_t *hikaru)
 	                            hikaru->bootrom, "BOOTROM/M");
 	vk_mmap_set_region (mmap, region, i++);
 
-	region = vk_region_nop_new (0x00400000, 0x00400001, 1,
-	                            VK_REGION_RW | VK_REGION_SIZE_ALL | VK_REGION_LOG_RW,
-	                            "UNK/M");
+	region = vk_region_mmio_new (0x00400000, 0x00400001, 1,
+	                            VK_REGION_RW | VK_REGION_SIZE_16 | VK_REGION_LOG_RW,
+	                            hikaru->gpu, "UNK/M");
 	vk_mmap_set_region (mmap, region, i++);
 
 	region = vk_region_mmio_new (0x00800000, 0x0083FFFF, 0x3FFFF,

src/mach/hikaru/hikaru-gpu-private.h
@@ -150,6 +150,7 @@ typedef struct {
 	uint8_t regs_1A[0x104];
 	uint8_t regs_1A_unit[2][0x40];
 	uint8_t regs_1A_fifo[0x10];
+	uint16_t unk_00400000;
 
 	unsigned frame_type;
 

src/mach/hikaru/hikaru.h
@@ -63,7 +63,6 @@ typedef struct {
 	unsigned porta_s_bit1_buffer;
 
 	/* Unknown Hardware (master) */
-	uint32_t unk00400000_m;
 	uint32_t unk01000000_m;
 	uint32_t unk01000100_m;
 

commit 6a7b289e4c89ec1a7844186486b460bce87bd54a

hikaru, hikaru/gpu: allow 32-bit access to 00400000.
Silences unhandled access warnings in AIRTRIX. (The value read from 00400000 is
always discarded, so there will likely be zero emulation regressions from this
change.)

Broken in ea8ebf904dc2da8805e3a7a342ba3f194df657f5.

src/mach/hikaru/hikaru-gpu.c
@@ -1014,7 +1014,7 @@ hikaru_gpu_get (vk_device_t *dev, unsigned size, uint32_t addr, void *val)
 
 	*val32 = 0;
 	if (addr == 0x00400000) {
-		*val16 = gpu->unk_00400000;
+		set_ptr (val, size, gpu->unk_00400000);
 	} else if (addr >= 0x15000000 && addr < 0x15000100) {
 		switch (addr & 0xFF) {
 		case 0x10:

src/mach/hikaru/hikaru.c
@@ -631,7 +631,7 @@ setup_master_mmap (hikaru_t *hikaru)
 	vk_mmap_add_region (mmap, region);
 
 	region = vk_region_mmio_new (0x00400000, 0x00400001, 1,
-	                            VK_REGION_RW | VK_REGION_SIZE_16 | VK_REGION_LOG_RW,
+	                            VK_REGION_RW | VK_REGION_SIZE_16 | VK_REGION_SIZE_32 | VK_REGION_LOG_RW,
 	                            hikaru->gpu, "UNK/M");
 	vk_mmap_add_region (mmap, region);
 